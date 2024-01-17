ae2898521803c16bd4d073bd78af0daa343b6786
Vlad Prudius <vladvlpr006@gmail.com>
2024-01-17 17:39:05 +0200

    added user created integration event

---
+++ DigiDent.Application/Shared/Abstractions/IIntegrationEvent.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace DigiDent.Application.Shared.Abstractions;
+
+public interface IIntegrationEvent: INotification
+{
+    Guid Id { get; }
+    DateTime TimeOfOccurrence { get; }
+}
---
+++ DigiDent.Application/UserAccess/Commands/SignUp/UserSignedUpEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using DigiDent.Domain.UserAccessContext.Users.Events;
+using MediatR;
+
+namespace DigiDent.Application.UserAccess.Commands.SignUp;
+
+public class UserSignedUpEventHandler: INotificationHandler<UserSignedUpDomainEvent>
+{
+    public Task Handle(UserSignedUpDomainEvent notification, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
