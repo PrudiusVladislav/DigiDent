48e1ee5ec04c06d95b9572da2a7e95a0b3596df6
Vlad Prudius <vladvlpr006@gmail.com>
2024-01-21 22:48:26 +0200

    added AllEmployeesRepository

---
+++ DigiDent.EFCorePersistence/ClinicCore/Employees/AllEmployeesRepository.cs
@@ -0,0 +1,14 @@
+﻿using DigiDent.Domain.ClinicCoreContext.Employees.Shared;
+using DigiDent.Domain.ClinicCoreContext.Employees.Shared.Abstractions;
+
+namespace DigiDent.EFCorePersistence.ClinicCore.Employees;
+
+public class AllEmployeesRepository : 
+    EmployeesRepository<Employee>,
+    IAllEmployeesRepository
+{
+    public AllEmployeesRepository(ClinicCoreDbContext context)
+        : base(context)
+    {
+    }
+}
