c4e5f724dc09c29ad39209d1b48aabad4d3b6b83
Vlad Prudius <vladvlpr006@gmail.com>
2024-01-23 13:57:19 +0200

    added RoleFactory

--- DigiDent.Domain/SharedKernel/ValueObjects/Role.cs
+++ DigiDent.Domain/SharedKernel/ValueObjects/Role.cs
@@ -1,4 +1,7 @@
-﻿namespace DigiDent.Domain.SharedKernel.ValueObjects;
+﻿using DigiDent.Domain.SharedKernel.ReturnTypes;
+using DigiDent.Domain.UserAccessContext.Users.Errors;
+
+namespace DigiDent.Domain.SharedKernel.ValueObjects;
 
 public enum Role
 {
@@ -6,4 +9,36 @@ public enum Role
     Doctor,
     Assistant,
     Patient
+}
+
+/// <summary>
+/// Factory that contains methods for creating and managing <see cref="Role"/> objects.
+/// </summary>
+public static class RoleFactory
+{
+    public static Result<Role> CreateRole(
+        string roleName, params Role[] allowedRoles)
+    {
+        var isRoleValid = Enum.TryParse<Role>(roleName, true, out var role);
+        if (!isRoleValid)
+        {
+            return Result.Fail<Role>(RoleErrors
+                .RoleIsNotValid(roleName));
+        }
+
+        if (allowedRoles.Length > 0 && !allowedRoles.Contains(role))
+        {
+            return Result.Fail<Role>(RoleErrors
+                .RoleIsNotAllowed(roleName));
+        }
+        
+        return Result.Ok(role);
+    }
+    
+    public static Role[] EmployeeRoles => new[]
+    {
+        Role.Administrator,
+        Role.Doctor,
+        Role.Assistant
+    };
 }
