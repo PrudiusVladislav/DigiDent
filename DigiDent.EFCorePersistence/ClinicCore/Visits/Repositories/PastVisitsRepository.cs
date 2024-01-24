7f39b3ad6a47cae4ac78aed680a050cefe5e0bae
Vlad Prudius <vladvlpr006@gmail.com>
2024-01-24 09:04:08 +0200

    implemented PastVisitsRepository

--- DigiDent.EFCorePersistence/ClinicCore/Visits/Repositories/PastVisitsRepository.cs
+++ DigiDent.EFCorePersistence/ClinicCore/Visits/Repositories/PastVisitsRepository.cs
@@ -1,6 +1,26 @@
-﻿namespace DigiDent.EFCorePersistence.ClinicCore.Visits.Repositories;
+﻿using DigiDent.Domain.ClinicCoreContext.Visits;
+using DigiDent.Domain.ClinicCoreContext.Visits.Abstractions;
+using DigiDent.Domain.ClinicCoreContext.Visits.ValueObjects.Ids;
+using Microsoft.EntityFrameworkCore;
 
-public class PastVisitsRepository
+namespace DigiDent.EFCorePersistence.ClinicCore.Visits.Repositories;
+
+public class PastVisitsRepository :
+    VisitsRepository<PastVisit, PastVisitId, Guid>, IPastVisitsRepository
 {
-    
+    private readonly ClinicCoreDbContext _context;
+    public PastVisitsRepository(ClinicCoreDbContext context)
+        : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<IReadOnlyCollection<PastVisit>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return (await _context.PastVisits
+                .Include(pv => pv.Doctor)
+                .Include(pv => pv.Patient)
+                .ToListAsync(cancellationToken))
+            .AsReadOnly();
+    }
 }
