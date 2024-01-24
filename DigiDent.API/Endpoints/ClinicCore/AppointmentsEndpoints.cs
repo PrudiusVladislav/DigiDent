eb05ca4d78d154faccbd47910ed8919c9d79148c
Vlad Prudius <vladvlpr006@gmail.com>
2024-01-24 13:20:45 +0200

    refactored AppointmentsEndpoints into methods

--- DigiDent.API/Endpoints/ClinicCore/AppointmentsEndpoints.cs
+++ DigiDent.API/Endpoints/ClinicCore/AppointmentsEndpoints.cs
@@ -11,60 +11,50 @@ public static class AppointmentsEndpoints
     public static RouteGroupBuilder MapAppointmentsEndpoints(
         this RouteGroupBuilder groupBuilder)
     {
-        groupBuilder.MapGroup("/appointments")
-            .MapCreateAppointmentEndpoint()
-            .MapCloseAppointmentEndpoint();
+        var appointmentsGroup = groupBuilder.MapGroup("/appointments");
+
+        appointmentsGroup.MapPost("/", CreateAppointment);
+        appointmentsGroup.MapPut("/{id}", CloseAppointment);
+           
         return groupBuilder;
     }
-    
-    private static IEndpointRouteBuilder MapCreateAppointmentEndpoint(
-        this IEndpointRouteBuilder app)
-    {
-        app.MapPost("/", async (
-            [FromBody]CreateAppointmentRequest request,
-            IMediator mediator,
-            CancellationToken cancellationToken) =>
-        {
-            var commandCreationResult = CreateAppointmentCommand
-                .CreateFromRequest(request);
-            
-            if (commandCreationResult.IsFailure)
-                return commandCreationResult.MapToIResult();
-            
-            var result = await mediator.Send(
-                commandCreationResult.Value!, cancellationToken);
 
-            return result.Match(
-                onFailure: _ => result.MapToIResult(),
-                onSuccess: id => Results.Created(
-                    $"/appointments/{id}", id));
-        });
-        
-        return app;
+    private static async Task<IResult> CreateAppointment(
+        [FromBody]CreateAppointmentRequest request,
+        IMediator mediator,
+        CancellationToken cancellationToken)
+    {
+        var commandCreationResult = CreateAppointmentCommand
+            .CreateFromRequest(request);
+            
+        if (commandCreationResult.IsFailure)
+            return commandCreationResult.MapToIResult();
+            
+        var result = await mediator.Send(
+            commandCreationResult.Value!, cancellationToken);
+
+        return result.Match(
+            onFailure: _ => result.MapToIResult(),
+            onSuccess: id => Results.Created(
+                $"/appointments/{id}", id));
     }
     
-    private static IEndpointRouteBuilder MapCloseAppointmentEndpoint(
-        this IEndpointRouteBuilder app)
+    private static async Task<IResult> CloseAppointment(
+        [FromRoute]Guid id,
+        [FromBody]CloseAppointmentRequest request,
+        IMediator mediator,
+        CancellationToken cancellationToken)
     {
-        app.MapPost("/{id:guid}/close", async (
-            [FromRoute]Guid id,
-            [FromBody]CloseAppointmentRequest request,
-            IMediator mediator,
-            CancellationToken cancellationToken) =>
-        {
-            var commandResult = CloseAppointmentCommand.CreateFromRequest(
-                id, request);
-            if (commandResult.IsFailure)
-                return commandResult.MapToIResult();
+        var commandResult = CloseAppointmentCommand.CreateFromRequest(
+            id, request);
+        if (commandResult.IsFailure)
+            return commandResult.MapToIResult();
             
-            var result = await mediator.Send(
-                commandResult.Value!, cancellationToken);
+        var result = await mediator.Send(
+            commandResult.Value!, cancellationToken);
 
-            return result.Match(
-                onFailure: _ => result.MapToIResult(),
-                onSuccess: () => Results.Ok());
-        });
-        
-        return app;
+        return result.Match(
+            onFailure: _ => result.MapToIResult(),
+            onSuccess: () => Results.Ok());
     }
 }
