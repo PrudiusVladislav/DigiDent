9c66c8b4266951c2c2c71b9e37f68f33f20ada68
Vlad Prudius <vladvlpr006@gmail.com>
2024-01-28 14:07:37 +0200

    added CreateUserTests

---
+++ tests/UnitTests/DigiDent.Domain.UnitTests/UserAccess/Users/CreateUserTests.cs
@@ -0,0 +1,30 @@
+﻿using DigiDent.Domain.SharedKernel.ValueObjects;
+using DigiDent.Domain.UnitTests.Shared;
+using DigiDent.Domain.UnitTests.Shared.ValueObjectsFactories;
+using DigiDent.Domain.UnitTests.UserAccess.Users.Extensions;
+using DigiDent.Domain.UnitTests.UserAccess.Users.PasswordTests;
+using DigiDent.Domain.UserAccessContext.Users;
+using DigiDent.Domain.UserAccessContext.Users.Events;
+
+namespace DigiDent.Domain.UnitTests.UserAccess.Users;
+
+public class CreateUserTests
+{
+    [Fact]
+    public void Create_WithValidData_ShouldReturnCreatedAndRaiseEvent()
+    {
+        // Arrange
+        var fullName = FullNameFactory.GetValidFullName();
+        var email = EmailFactory.GetValidEmail();
+        var phoneNumber = PhoneNumberFactory.GetValidPhoneNumber();
+        var password = PasswordFactory.GetValidPassword();
+        var role = Role.Administrator;
+        
+        // Act
+        var user = User.Create(fullName, email, phoneNumber, password, role);
+        
+        // Assert
+        user.ShouldBeCreatedFrom(fullName, email, phoneNumber, password, role);
+        user.ShouldRaiseDomainEvent<UserSignedUpDomainEvent>();
+    }
+}
