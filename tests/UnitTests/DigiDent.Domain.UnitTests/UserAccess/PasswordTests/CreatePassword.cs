652cf55f88836f5b3336cc2608386f9c3fe657ed
Vlad Prudius <vladvlpr006@gmail.com>
2024-01-28 13:24:42 +0200

    added CreatePassword unit tests

---
+++ tests/UnitTests/DigiDent.Domain.UnitTests/UserAccess/PasswordTests/CreatePassword.cs
@@ -0,0 +1,45 @@
+﻿using DigiDent.Domain.SharedKernel.ReturnTypes;
+using DigiDent.Domain.UserAccessContext.Users.ValueObjects;
+using FluentAssertions;
+
+namespace DigiDent.Domain.UnitTests.UserAccess.PasswordTests;
+
+public class CreatePassword
+{
+    [Theory]
+    [MemberData(nameof(CreateValidPasswords))]
+    public void Create_WithValidPassword_ShouldCreateAndReturnPassword(string validPassword)
+    {
+        // Act
+        Result<Password> passwordResult = Password.Create(validPassword);
+
+        // Assert
+        passwordResult.IsSuccess.Should().BeTrue();
+        passwordResult.Value.Should().NotBeNull();
+        passwordResult.Value?.PasswordHash.Should().NotBeNullOrWhiteSpace();
+    }
+    
+    [Theory]
+    [MemberData(nameof(CreateInvalidPasswords))]
+    public void Create_WithInvalidPassword_ShouldReturnFailure(string invalidPassword)
+    {
+        // Act
+        Result<Password> passwordResult = Password.Create(invalidPassword);
+
+        // Assert
+        passwordResult.IsFailure.Should().BeTrue();
+        passwordResult.Value.Should().BeNull();
+    }
+    
+    public static IEnumerable<object[]> CreateValidPasswords()
+    {
+        return PasswordUtils.ValidPasswords
+            .Select(validPassword => (object[])[validPassword]);
+    }
+    
+    public static IEnumerable<object[]> CreateInvalidPasswords()
+    {
+        return PasswordUtils.InvalidPasswords
+            .Select(invalidPassword => (object[])[invalidPassword]);
+    }
+}
---
+++ tests/UnitTests/DigiDent.Domain.UnitTests/UserAccess/PasswordTests/PasswordUtils.cs
@@ -0,0 +1,29 @@
+﻿
+namespace DigiDent.Domain.UnitTests.UserAccess.PasswordTests;
+
+public static class PasswordUtils
+{
+    public static HashSet<string> ValidPasswords =>
+        [
+            "correcthorsebatterystaple",
+            "neverforget13/3/1997",
+            "do you know",
+            "ryanhunter2000"
+        ];
+
+    public static HashSet<string> InvalidPasswords =>
+        [
+            "",
+            "qwerty",
+            "password",
+            "Tr0ub4dour&3",
+            "abcdefghijk987654321",
+            "12345678"
+        ];
+
+    public static IEnumerable<(string, string, bool)> PasswordsEqualityExpectations => 
+        [
+            (ValidPasswords.First(), ValidPasswords.First(), true),
+            (ValidPasswords.First(), ValidPasswords.Last(), false)
+        ];
+}
