ad2e4de1a98f4641aed5f7080a4974afcff5013f
Vlad Prudius <vladvlpr006@gmail.com>
2024-01-29 07:42:45 +0200

    added unit tests for VisitDateTime.Create()

---
+++ tests/UnitTests/DigiDent.Domain.UnitTests/ClinicCore/Visits/TestUtils/Constants/VisitDateTimeConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace DigiDent.Domain.UnitTests.ClinicCore.Visits.TestUtils.Constants;
+
+public class VisitDateTimeConstants
+{
+    public static DateTime AnyDateTime = new(2024, 1, 1);
+    public static TimeSpan DefaultVisitDateTimeOffset = TimeSpan.FromHours(1);
+}
