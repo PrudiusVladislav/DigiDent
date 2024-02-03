edac11016a056f0283eff13450fafa87efc16e4d
Vlad Prudius <vladvlpr006@gmail.com>
2024-02-03 13:32:12 +0200

    added User.Activate() and domain event for it

---
+++ src/Modules/UserAccess/DigiDent.UserAccess.Domain/Users/Events/UserActivatedDomainEvent.cs
@@ -0,0 +1,8 @@
+﻿using DigiDent.Shared.Kernel.Abstractions;
+
+namespace DigiDent.UserAccess.Domain.Users.Events;
+
+public record UserActivatedDomainEvent(
+    Guid EventId,
+    DateTime TimeOfOccurrence,
+    User ActivatedUser): IDomainEvent;
