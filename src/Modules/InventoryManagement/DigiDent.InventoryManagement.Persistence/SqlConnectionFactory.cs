fed140b22ba480eaf68ca28bb734ab3d55aa905c
Vlad Prudius <vladvlpr006@gmail.com>
2024-02-18 15:47:04 +0200

    added IDbConnectionFactory and SqlConnectionFactory implementation

---
+++ src/Modules/InventoryManagement/DigiDent.InventoryManagement.Persistence/SqlConnectionFactory.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using DigiDent.Shared.Abstractions.Factories;
+using Microsoft.Data.SqlClient;
+
+namespace DigiDent.InventoryManagement.Persistence;
+
+public class SqlConnectionFactory
+    : IDbConnectionFactory<SqlConnection>
+{
+    private readonly string _connectionString;
+    
+    public SqlConnectionFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+    
+    public SqlConnection CreateConnection()
+    {
+        return new SqlConnection(_connectionString);
+    }
+    
+    public SqlConnection CreateOpenConnection()
+    {
+        var connection = CreateConnection();
+        connection.Open();
+        return connection;
+    }
+}
---
+++ src/Shared/DigiDent.Shared.Abstractions/Factories/IDbConnectionFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+namespace DigiDent.Shared.Abstractions.Factories;
+
+public interface IDbConnectionFactory<out TConnection>
+    where TConnection: IDbConnection
+{
+    TConnection CreateConnection();
+    TConnection CreateOpenConnection();
+}
