b22fc977d069ffa09d104aace6ece4ee22f989e2
Vlad Prudius <vladvlpr006@gmail.com>
2024-02-23 23:07:07 +0200

    added mapping of related entities in ActionsQueryRepository

--- src/Modules/InventoryManagement/DigiDent.InventoryManagement.Persistence/Actions/InventoryActionsQueriesRepository.cs
+++ src/Modules/InventoryManagement/DigiDent.InventoryManagement.Persistence/Actions/InventoryActionsQueriesRepository.cs
@@ -2,10 +2,13 @@
 using DigiDent.InventoryManagement.Domain.Actions;
 using DigiDent.InventoryManagement.Domain.Actions.ReadModels;
 using DigiDent.InventoryManagement.Domain.Actions.ValueObjects;
+using DigiDent.InventoryManagement.Domain.Employees;
+using DigiDent.InventoryManagement.Domain.Items.ReadModels;
 using DigiDent.InventoryManagement.Domain.Items.ValueObjects;
 using DigiDent.InventoryManagement.Persistence.Constants;
-using DigiDent.InventoryManagement.Persistence.Shared;
-using DigiDent.Shared.Kernel.ValueObjects.Pagination;
+using DigiDent.Shared.Infrastructure.Persistence.Factories;
+using DigiDent.Shared.Kernel.Extensions;
+using DigiDent.Shared.Kernel.Pagination;
 
 namespace DigiDent.InventoryManagement.Persistence.Actions;
 
@@ -23,37 +26,49 @@ public class InventoryActionsQueriesRepository: IInventoryActionsQueriesReposito
         IPaginationOptions pagination, CancellationToken cancellationToken)
     {
         const string schema = ConfigurationConstants.InventoryManagementSchema;
-        const string sql = $@"
-            SELECT a.[Id], a.[Type], a.[Quantity], a.[Date], 
-            e.[Id] AS [EmployeeId], e.[Name] AS [EmployeeName], 
-            i.[Id] AS [ItemId], i.[Name] AS [ItemName], i.[Category] AS [ItemCategory]
-            FROM {schema}.[Actions] a 
-                JOIN {schema}.[Employees] e ON a.[ActionPerformerId] = e.[Id]
-                JOIN {schema}.[Items] i ON a.[InventoryItemId] = i.[Id]
+        const string query = $@"
+            SELECT
+                a.[Id] AS ActionId,
+                a.[Type] AS ActionType,
+                a.[Quantity] AS ActionQuantity,
+                a.[Date] AS ActionDate,
+                e.[Id] AS EmployeeId,
+                e.[FullName] AS EmployeeFullName,
+                i.[Id] AS InventoryItemId,
+                i.[Name] AS InventoryItemName,
+                i.[Quantity] AS InventoryItemQuantity,
+                i.[Category] AS InventoryItemCategory
+            FROM 
+                {schema}.[Actions] a
+                INNER JOIN {schema}.Employees e ON a.EmployeeId = e.Id
+                INNER JOIN {schema}.InventoryItems i ON a.InventoryItemId = i.Id
             ORDER BY a.[Date] @SortDirection
             WHERE a.[Id] > @Cursor
             LIMIT @PageSize";
         
         await using var connection = _connectionFactory.CreateOpenConnection();
         IReadOnlyCollection<ActionSummary> actions = (await connection
-                .QueryAsync<ActionSummary>(
-                    sql, new
-                    { 
+                .QueryAsync<ActionSummary, EmployeeSummary, InventoryItemSummary, ActionSummary>(
+                    sql: query, 
+                    map: (action, employee, inventoryItem) => action with
+                    {
+                        Type = Enum.Parse<ActionType>(action.Type).ToString(),
+                        ActionPerformer = employee,
+                        InventoryItem = inventoryItem with 
+                        {
+                            Category = Enum
+                                .Parse<ItemCategory>(inventoryItem.Category)
+                                .ToString()
+                        }
+                    }, 
+                    splitOn: "EmployeeId, InventoryItemId",
+                    param: new
+                    {
                         SortDirection = pagination.SortOrder,
                         Cursor = pagination.PageSize * (pagination.PageNumber - 1),
                         PageSize = pagination.PageSize
                     }))
-            .Select(action => action with
-            {
-                Type = Enum
-                    .Parse<ActionType>(action.Type)
-                    .ToString(),
-                
-                ItemCategory = Enum
-                    .Parse<ItemCategory>(action.ItemCategory)
-                    .ToString()
-            })
-            .Where(action => action.Contains(pagination.SearchTerm))
+            .Filter(pagination.SearchTerm)
             .AsList()
             .AsReadOnly();
         
