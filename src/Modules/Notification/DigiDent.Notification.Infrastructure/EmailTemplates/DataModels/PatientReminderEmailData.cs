85375d2201e2e963fc41d39d11071e61288139c7
Vlad Prudius <vladvlpr006@gmail.com>
2024-03-05 09:49:52 +0200

    refactored PatientReminderEmail to razor

---
+++ src/Modules/Notification/DigiDent.Notification.Infrastructure/EmailTemplates/DataModels/PatientReminderEmailData.cs
@@ -0,0 +1,7 @@
+﻿namespace DigiDent.Notification.Infrastructure.EmailTemplates.DataModels;
+
+public record PatientReminderEmailData(
+    string PatientName,
+    string DoctorName,
+    DateOnly AppointmentDate,
+    TimeOnly AppointmentTime);
